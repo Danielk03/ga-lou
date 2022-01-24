2878c7ee7c660f086c3bf7c3c6f58f19bf3dab5a
dankal505 <dankal505@edu.linkoping.se>
2022-01-24 13:09:38 +0100

    testar heroku

---
+++ config.php
@@ -0,0 +1,7 @@
+<?php
+
+$host= 'ec2-54-229-47-120.eu-west-1.compute.amazonaws.com';
+$db = 'd594norvgo23tp';
+$user = 'vevhfojrdqecgr';
+$password = '89d412566309bc11219cfda4c3809f6ccd4dd2fd017805f3047c0ef5072fc776'; // change to your password
+
