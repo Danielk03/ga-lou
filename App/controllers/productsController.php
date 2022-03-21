c40f118202ffdcd99e4305c09a47bac3071abf63
emiduc082 <emiduc082@edu.linkoping.se>
2022-03-21 13:06:56 +0100

    ME

--- App/controllers/productsController.php
+++ App/controllers/productsController.php
@@ -26,42 +26,54 @@ class productsController
         </head>
         <body>
         <div class="containerborder">
+            <div class="profile-icon-placement">
+                <svg xmlns="http://www.w3.org/2000/svg" fill="currentColor" class="bi bi-person-circle" viewBox="0 0 16 16">
+                    <path d="M11 6a3 3 0 1 1-6 0 3 3 0 0 1 6 0z"/>
+                    <path fill-rule="evenodd" d="M0 8a8 8 0 1 1 16 0A8 8 0 0 1 0 8zm8-7a7 7 0 0 0-5.468 11.37C3.242 11.226 4.805 10 8 10s4.757 1.225 5.468 2.37A7 7 0 0 0 8 1z"/>
+                </svg>
+            </div>
             <nav class="navbar navbar-light nav-icon-placement">
                 <div class="container-fluid">
-                    <button class="navbar-toggler" type="button" data-bs-toggle="collapse"
-                            data-bs-target="#navbarToggleExternalContent" aria-controls="navbarToggleExternalContent"
-                            aria-expanded="false" aria-label="Toggle navigation">
+                    <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarToggleExternalContent" aria-controls="navbarToggleExternalContent" aria-expanded="false" aria-label="Toggle navigation">
                         <span class="navbar-toggler-icon"></span>
                     </button>
                 </div>
             </nav>
             <br>
+            <br><br><br>
+            <div class="row row-cols-2 row-cols-md-2 g-4">
+                <div class="col">
+                    <div class="card h-100">
+                        <img src="/uploadsoctane_3_4x.png" class="card-img-top" alt="KO" style="height: 45%">
+                        <div class="card-body">
+                            <h5 class="card-title">Card title</h5>
+                            <p class="card-text">This card has supporting text below as a natural lead-in to additional content.</p>
+                            <p class="card-text"><small class="text-muted">Last updated 3 mins ago</small></p>
+                        </div>
+                    </div>
+                </div>
+                <div class="col">
+                    <div class="card h-100">
+                        <img src="/uploadsgolden-gate.jpg" class="card-img-top" alt="..." style="height: 45%">
+                        <div class="card-body">
+                            <h5 class="card-title">Card title</h5>
+                            <p class="card-text">This card has supporting text below as a natural lead-in to additional content.</p>
+                            <p class="card-text"><small class="text-muted">Last updated 3 mins ago</small></p>
+                        </div>
+                    </div>
+                </div>
+            </div>
             <div class="collapse" id="navbarToggleExternalContent">
-                <div class="container container-nav" id="container">
-                    <button class="navbar-toggler nav-icon-close" type="button" data-bs-toggle="collapse"
-                            data-bs-target="#navbarToggleExternalContent" aria-controls="navbarToggleExternalContent"
-                            aria-expanded="false" aria-label="Toggle navigation"> CLOSE
-                    </button>
+                <div class="container-nav" id="container">
+                    <button class="navbar-toggler nav-icon-close" type="button" data-bs-toggle="collapse" data-bs-target="#navbarToggleExternalContent" aria-controls="navbarToggleExternalContent" aria-expanded="false" aria-label="Toggle navigation"> CLOSE</button>
                     <br>
-                    <p class="nav-font"><a href="/products/">produkter</a></p>
-                    <p class="nav-font"><a href="/products/user">Dina produkter</a></p>
+                    <p class="nav-font">Hitta produkter</p>
+                    <p class="nav-font">Dina produkter</p>
                     <p></p>
                 </div>
             </div>
             <!-- fixa så att knappen tar den till sin profil eller log in -->
-            <div class="profile-icon-placement">
-                <svg xmlns="http://www.w3.org/2000/svg" fill="currentColor" class="bi bi-person-circle"
-                     viewBox="0 0 16 16">
-                    <path d="M11 6a3 3 0 1 1-6 0 3 3 0 0 1 6 0z"/>
-                    <path fill-rule="evenodd"
-                          d="M0 8a8 8 0 1 1 16 0A8 8 0 0 1 0 8zm8-7a7 7 0 0 0-5.468 11.37C3.242 11.226 4.805 10 8 10s4.757 1.225 5.468 2.37A7 7 0 0 0 8 1z"/>
-                </svg>
-            </div>
-            <i class="bi bi-person-circle">Person</i>
-            <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js"
-                    integrity="sha384-ka7Sk0Gln4gmtz2MlQnikT1wXgYsOg+OMhuP+IlRH9sENBO0LRn5q+8nbTov4+1p"
-                    crossorigin="anonymous"></script>
-        </div>
+            <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ka7Sk0Gln4gmtz2MlQnikT1wXgYsOg+OMhuP+IlRH9sENBO0LRn5q+8nbTov4+1p" crossorigin="anonymous"></script>
         </body>
         </html>
 
@@ -115,14 +127,27 @@ class productsController
             </label>
         </body>
 
+        <div class="row row-cols-2 row-cols-md-2 g-4">
          <?php
         foreach ($products as $product) {
-            echo '<h4>' . $product->productTitle . '</h4>';
-            echo '<ol><a href="/products/details/' . $product->productId . '"> läs mer om ' . $product->productTitle, '<a/></ol> ';
-            echo '<h5> Säljare : ' . $product->username . '</h5> <br>';
+            echo
+                '<div class="col">
+        <div class="card h-100">
+            <img src="/upload/1.jpg" class="card-img-top" alt="KO" style="height: 45%">
+            <div class="card-body">
+                <h5 class="card-title">'. $product->productTitle.'</h5>
+                <p class="card-text"> '.$product->productDescription.'</p>
+                <p class="card-text"><small class="text-muted">'.$product->uploadDate.'</small></p>
+            </div>
+            </div>
+        </div>
+        
+       ';
         }
-
-    }
+        ?>
+        </div>
+    <?php
+        }
 
     public function userProduct()
     {
