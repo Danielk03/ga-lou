b10b4304f6e3fe93885bb6589136bfd608b4748a
emiduc082 <emiduc082@edu.linkoping.se>
2022-05-06 09:54:55 +0200

    html i funktioner & oinloggade kan inte komma in på vad de vill 2022-05-02 // EMIL

--- App/controllers/indexController.php
+++ App/controllers/indexController.php
@@ -10,18 +10,9 @@ class indexController
     {
         ?>
         <!doctype html>
-        <html lang="sv">
-        <head>
-            <meta charset="UTF-8">
-            <meta name="viewport"
-                  content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
-            <meta http-equiv="X-UA-Compatible" content="ie=edge">
-            <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet"
-                  integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3"
-                  crossorigin="anonymous">
-            <link rel="stylesheet" href="index.css?v=1">
-            <title>Document</title>
-        </head>
+       <?php
+       head();
+       ?>
         <body>
         <div class="containerborder">
             <div class="center m-above">
@@ -41,8 +32,7 @@ class indexController
                 </form>
             </div>
         </div>
-        </body>
-        </html>
         <?php
+        endTags();
     }
 }
